84d4a779bff6e8e5dce780b7ed4fc640fc7e4f4b
henry <learnubuntuandprogramming@gmail.com>
2026-03-26 10:30:02 +0000

    第二版：修复公式语法并生成Excel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from openpyxl import Workbook
+from openpyxl.styles import Font
 
 # 第一版本，只用 openpyxl 功能 sum(),不用loop只看计算结果
 workbook = Workbook()
@@ -16,11 +17,9 @@ worksheet.append(["盒裝紙巾無味18盒裝",10,124.00])
 
 # 计算 Total，显示结果，从最后一笔留空一行
 worksheet['A5'] = "总共消费 HKD"
-worksheet['A5'].font = Font(
-    name="Arial",
-    size=14,
-    bold=True
-)
+worksheet['A5'].font = Font(name="Arial",size=14,bold=True)
+
 # 单一消费 = cellB x cellC
 # 总数 = 所有 单一消费加起来
-worksheet['B5'] = "=Sum(B2:B3) * Sum(c2:c3)"
+worksheet['B5'] = "=Sum(B2:B3)*Sum(c2:c3)" #
+workbook.save("product_list.xlsx")
